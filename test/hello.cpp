87ed85ceba40996aa1379d70ff1436fce6237274
Samridhi Thakur <samridhithakur5378@gmail.com>
2026-08-11 23:51:31 +0530

    first commit

---
+++ test/hello.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+//# se start hone wile line ko bola jata ahi preprocessor director
+
+using namespace std;
+
+using std::cout;
+using std::endl;
+
+int main(){
+cout << "hello chai from samridhi" ;
+return 0 ;
+}
+//statically typed you ahev to define the type of value
