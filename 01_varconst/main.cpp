621eba1705f2abd38dade62ed015385291f0c139
Samridhi Thakur <samridhithakur5378@gmail.com>
2026-08-13 00:56:03 +0530

    day3

---
+++ 01_varconst/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+  int score;//declare integer
+  score= 110;
+
+  const int uid =232323;
+  int hiteshbalance = 500; 
+  hiteshbalance = 1000;
+ // uid = 1223;
+  cout << "welcome to cahi aur cpp" << endl ; 
+return 0;
+}
+//single line comment
+/*multi line comment*/
+//lvalue left handside value
+//rvalue right handside value
