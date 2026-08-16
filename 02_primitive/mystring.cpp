4284f2b9afaad0f778afe6c4f201ecc216aecb43
Samridhi Thakur <samridhithakur5378@gmail.com>
2026-08-16 11:07:36 +0530

    .

--- 02_primitive/mystring.cpp
+++ 02_primitive/mystring.cpp
@@ -4,8 +4,16 @@
 using namespace std;
 
 int main(){
- string favouriteTea = "Lemon Tea ";
- string description = "known as  \"best\" tea";//escape character(\) programamtic effect hta deta hai
- cout << favouriteTea << endl;
+string userTea;
+int teaQuantity;
+
+cout <<"which tea would you like to order?\n";
+getline(cin,userTea);
+
+//ask for quantity
+cout << "how many cups of"<< userTea << "would you like to have?";
+cin >> teaQuantity;
+cout << teaQuantity;
+cout <<userTea;
   return 0;
 }
