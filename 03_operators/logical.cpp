2f6ddbdb61dc768c1350f3545a373fc57ae4faa7
Samridhi Thakur <samridhithakur5378@gmail.com>
2026-08-18 00:39:37 +0530

    logical operator

---
+++ 03_operators/logical.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+ int main(){
+bool isstudent;
+cout << "are you a student(1 for yes and 0 for no)";
+cin >> isstudent;
+int teaCups;
+cout << "how many cups have you purchased ";
+cin  >> teaCups;
+if(isstudent || teaCups >15){
+  cout << "you are eligible for a discount" << endl;
+}else{
+  cout << "you are not eligible for a discount" << endl;
+}
+  return 0;
+ }
