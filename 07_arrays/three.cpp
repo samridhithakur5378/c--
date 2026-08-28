22e5d3577596c674f577c292d4f31ece5efbedb5
Samridhi Thakur <samridhithakur5378@gmail.com>
2026-08-28 10:27:06 +0530

    task one wto and three done

---
+++ 07_arrays/three.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int totalChaiServed(int chai[],int size){
+  int total =0;
+  for(int i=0;i< size : i++){
+    total += chai[i];
+
+  }
+  return total;
+}
+int main(){
+  int chaiServed[7]= {50,60,55,70,65,80,75};
+
+  int total = totalChaiServed(chaiServed,7); 
+
+
+  return 0;
+}
+//didnt quite understood cehck again
---
+++ 07_arrays/two.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+int chaiServed[7] ={50,60,55,70,65,80,75}
+
+cout << " chai cups served on day 1:" << chaiServed[0];
+
+cout << " chai cups served on day 2:" << chaiServed[1];
+
+
+
+
+  return 0;
+}
